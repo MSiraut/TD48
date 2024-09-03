0a894c9a300519b5eaee9cec786989c1e97eac8e
alex <adulas477@gmail.com>
2024-09-03 12:15:50 +0200

    main test du td

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CC personne = new CC("dulas","alexandre",3);
-        System.out.println(personne.ajouter(4));
+        System.out.println(personne.ajouter(14));
 
     }
 }
